3296143483d6c8290ae07695c6ec12a5dce44ed7
KetanBorsee <168560130+KetanBorsee@users.noreply.github.com>
2024-11-07 00:10:43 +0530

    Delete pre-interview directory

--- pre-interview/interview.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const video = document.getElementById("interviewVideo");
-
-    setTimeout(() => {
-        video.play();
-    }, 5000);
-});
